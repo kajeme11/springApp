905f25c68479646dd0d647bb0a8b8ddcbffb6425
kajeme11 <chenequek@gmail.com>
2025-04-15 23:33:59 -0700

    bean life cycle

--- src/main/java/com/springApp/proj/service/greet/Greetings.java
+++ src/main/java/com/springApp/proj/service/greet/Greetings.java
@@ -6,7 +6,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class Greetings {
 
+    static {
+        System.out.println("Static block in Greetings");
+    }
+
+    {
+        System.out.println("non-static block in Greetings");
+    }
+
     public Greetings(){
-        System.out.println("Greetings!!!!");
+        System.out.println("Constructor from Greetings!!!!");
+    }
+
+    public String greet(){
+        return "Good Morning!";
     }
 }
