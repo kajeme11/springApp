21e51a865f62e8daf83f51ada5f3c4348c3aad07
kajeme11 <chenequek@gmail.com>
2025-04-15 23:21:23 -0700

    springboot preconfiguration

--- src/main/java/com/springApp/proj/service/SpringBoot.java
+++ src/main/java/com/springApp/proj/service/SpringBoot.java
@@ -2,11 +2,22 @@ package com.springApp.proj.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication  // = @ComponentScan + @EnableAutoConfiguration
 public class SpringBoot {
     public static void main(String[] args) {
-        SpringApplication.run(SpringBoot.class, args);
+
+
+
+//        SpringApplication.run(SpringBoot.class, args);
+        ConfigurableApplicationContext container =  SpringApplication.run(SpringBoot.class, args);
+
+
+        //IOC has 52 beans by default
+        //In addition to beans created by developer or dependecies
+        //Get the count of beans in the container
+        System.out.println("There are " + container.getBeanDefinitionCount() + " beans in the spring container");
     }
 
 }
---
+++ src/main/java/com/springApp/proj/service/greet/Greetings.java
@@ -0,0 +1,12 @@
+package com.springApp.proj.service.greet;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Greetings {
+
+    public Greetings(){
+        System.out.println("Greetings!!!!");
+    }
+}
